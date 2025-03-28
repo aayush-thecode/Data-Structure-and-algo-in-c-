efb8d02d2e8d7ce404f0cdb7c9d68c01b7de1d70
Aayush Pradhan <muma5581@gmail.com>
2025-03-28 23:18:05 +0545

    patterns

---
+++ patterns.cpp
@@ -0,0 +1,20 @@
+// question 1:
+#include <iostream>
+using namespace std;
+
+int main () {
+int n = 5;
+bool val = true; 
+
+
+for(int i = 0; i<n; i++){
+    for(int j = 0; j<=i; j++){
+        cout<<val<<"";
+        val = !val;
+    }
+    cout << "\n";
+}
+
+
+    return 0; 
+}

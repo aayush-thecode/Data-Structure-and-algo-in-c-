2c6f6d315e4be5c2c361153e5834fbcbb7d64d4f
Aayush Pradhan <muma5581@gmail.com>
2025-04-27 09:06:18 +0545

    remove dubplicate second approach

--- recursion/remove.duplicates.cpp
+++ recursion/remove.duplicates.cpp
@@ -2,11 +2,11 @@
 #include <string>
 using namespace std;
 
-void removeDuplicates(string str, string ans, int i,int map[26]){
+int removeDuplicates(string str, string ans, int i,int map[26]){
 // base case
     if(i == str.size()) {
         cout << "ans : " << ans << endl;
-        return;
+        return 0;
     }
 
     char ch = str[i];
@@ -16,6 +16,7 @@ void removeDuplicates(string str, string ans, int i,int map[26]){
         removeDuplicates(str, ans, i+1, map);
     } else { //not duplicate
         map[mapIdx] = true;
+        removeDuplicates(str, ans + str[i], i+1,  map);
     }
 };
 
@@ -26,4 +27,25 @@ int main(){
 
     removeDuplicates(str, ans, 0, map);
     return 0;
+}
+
+// second approach 
+
+void removeDuplicates(string str, string ans, int map[26]) {
+    if(str.size() == 0) {
+        cout << "ans" << ans <<endl;
+        return;
+    }
+
+    int n = str.size();
+    char ch = str[n-1];
+    int mapIdx = (int)(str[n-1] - 'a');
+    str = str.substr(0, n-1);
+
+    if(map[mapIdx]) {
+        removeDuplicates(str, ans, map);
+    } else {
+        map[mapIdx] = true;
+        removeDuplicates(str, ch + ans, map);
+    }
 }

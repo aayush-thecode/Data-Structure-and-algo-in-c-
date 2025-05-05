406859e63a4faede9d3ed8173bce4aedfccc73cc
Aayush Pradhan <muma5581@gmail.com>
2025-05-05 07:43:17 +0545

    permutations-- backtracking concept

---
+++ backtracking/permutation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void permutations(string str, string ans) {
+    int n = str.size();
+    
+    //base case
+    if(n == 0) {
+        cout << ans << "\n";
+        return;
+    }
+
+    for(int i = 0; i<str.size(); i++){
+        char ch= str[i];
+        // "abcdefgh", i = 2 => "ab" + "defgh" => str.substr(0,i) + str.substr(i + 1)
+        string nextStr = str.substr(0, i) + str.substr(i+1, n-i-1);
+        permutations(nextStr, ans + ch); //ith char choice to add in permutation
+    }
+};
+
+int main() {
+    string str = "abc";
+    string ans = "";
+
+    permutations(str, ans);
+
+    return 0;
+}

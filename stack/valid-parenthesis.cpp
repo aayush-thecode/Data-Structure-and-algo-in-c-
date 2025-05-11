a21d536d2eb0d41cb1abc5ed490f1871ea2ecc41
Aayush Pradhan <muma5581@gmail.com>
2025-05-11 22:21:00 +0545

    duplicate parentesis

--- stack/valid-parenthesis.cpp
+++ stack/valid-parenthesis.cpp
@@ -30,13 +30,37 @@ bool isValid(string str) {
     return s.empty();
 }
 
+bool isDuplicate(string str) {
+    stack<char> s;
+
+    for(int i = 0; i < str.size(); i++) {
+        char ch = str[i];
+        if(ch != ')') { //non-closing
+            s.push(ch);
+        } else {
+            if(s.top() == '(') { //dupliate
+                return true;
+            }
+            
+            while(s.top() != '(') {
+                s.pop();
+            }
+            s.pop();
+        }
+    }
+    return false;
+}
+
 
 int main() {
-    string str1 = "([}])"; //invalid: false
-    string str2 = "([{}])"; //valid: true
+    // string str1 = "([}])"; //invalid: false
+    // string str2 = "([{}])"; //valid: true
 
-    cout << isValid(str1) << endl;
-    cout << isValid(str2) << endl;
+    string str1 = "((a+b))"; // invalid : true
+    string str2 = "((a+b) + (c+d))"; //valid : 1
+
+    cout << isDuplicate(str1) << endl;
+    cout << isDuplicate(str2) << endl;
 
     return 0;
 }

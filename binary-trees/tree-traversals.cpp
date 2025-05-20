9785aaf08c2f2ae458653e7aa20c3435e5e7cb13
Aayush Pradhan <muma5581@gmail.com>
2025-05-20 20:48:34 +0545

    binary trees-- sum of nodes binary tree

--- binary-trees/tree-traversals.cpp
+++ binary-trees/tree-traversals.cpp
@@ -120,6 +120,18 @@ int count(Node* root) {
     return leftCount + rightCount + 1;
 }
 
+int sum(Node* root) {
+    if(root == NULL) {
+        return 0;
+    }
+
+    int leftSum = sum(root->left);
+    int rightSum = sum(root->right);
+
+    return leftSum + rightSum + root->data;
+
+}
+
 int main() {
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     int idx = 0;
@@ -140,5 +152,7 @@ int main() {
     cout << "height : " << height(root) << endl;
 
     cout << "count : " << count(root) << endl;
+
+    cout << "Sum of nodes : " << sum(root) << endl;
     return 0;
 }

756b922f66acc26581bf9186548bfd19244b2278
Aayush Pradhan <muma5581@gmail.com>
2025-05-24 22:24:00 +0545

    binary search tree build a BST from Arrat

---
+++ binary-trees/binary search tree/build-BSA-Array.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val) { //O(logn)
+    if(root == NULL) {
+        root = new Node(val);
+        return root;
+    }
+
+    if(val < root->data) { // left-subtree
+        root->left = insert(root->left, val);
+    } else { // right sub-tree
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+Node* buildBST(int arr[], int n) {
+    Node* root = NULL;
+
+    for(int i = 0; i<n; i++) {
+        root = insert(root, arr[i]);
+    }
+    return root;
+}
+
+void inOrder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+    
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main() {
+    int arr[6] = {5, 1, 3, 4, 2, 7};
+    //int arr[9] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+    
+    Node* root = buildBST(arr, 6);
+    inOrder(root);
+    cout << endl;
+    return 0;
+}

ded562e4aead42f1f1101806d1872f662d54229a
Aayush Pradhan <muma5581@gmail.com>
2025-05-26 21:36:42 +0545

    binary search tree inorder successor

--- binary-trees/binary search tree/build-BSA-Array.cpp
+++ binary-trees/binary search tree/build-BSA-Array.cpp
@@ -63,14 +63,53 @@ bool search(Node* root,int key) { //O(height); avg )(logn)
     }
 }
 
-int main() {
-    int arr[6] = {5, 1, 3, 4, 2, 7};
-    // int arr[9] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
-    
-    Node* root = buildBST(arr, 6);
-    inOrder(root);
-    cout << endl;
-
-    cout << search(root, 15) << endl;
-    return 0;
+Node* getInorderSuccessor(Node* root) {
+    while(root->left != NULL){
+        root = root->left;
+    }
+    return root; //IS
 }
+
+Node* delNode(Node* root, int val) {
+    if(root == NULL) {
+        return NULL;
+    }
+
+    if(val < root->data) { // left subtree
+        root->left = delNode(root->left, val);
+    } else if(val > root->data) { // right subtree
+        root->right = delNode(root->right, val);
+    } else {
+        //root == va;
+        //case1 : 0 children
+        if(root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+
+        //case 2 : 1 chile
+        if(root->left == NULL ||  root->right == NULL) {
+            return root->left == NULL ? root->right : root->left; 
+        }
+
+        //case 3 : 2 children
+        Node* IS = getInorderSuccessor(root->right);
+        root->data = IS->data;
+        root->right = delNode(root->right, IS->data); //case 1, case2
+        return root;
+    }
+    return root;
+}
+int main() {
+    // int arr[6] = {5, 1, 3, 4, 2, 7};
+    int arr[9] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+    
+    Node* root = buildBST(arr, 9);
+    
+    inOrder(root);
+    cout<< endl;
+
+    delNode(root, 10);
+    inOrder(root);
+    return 0;
+};

3c2fd5dade767e220d9da4e06a4f963041028eb2
Aayush Pradhan <muma5581@gmail.com>
2025-05-27 09:38:15 +0545

    binary search tree BST to balance BST

--- binary-trees/BST_2/code.cpp
+++ binary-trees/BST_2/code.cpp
@@ -38,12 +38,58 @@ void preorder(Node* root) {
     preorder(root->right);
 };
 
-int main() {
-    int arr[7] = {3, 4, 5, 6, 7, 8, 9};
+Node* buildBSTfromSortedVector(vector<int> arr, int st, int end) {
+    if(st > end) {
+        return NULL;
+    }
 
-    Node* root = buildBSTfromSorted(arr, 0, 6);
+    int mid = st + (end-st) /2;
+    Node*curr = new Node(arr[mid]);
+
+    curr->left = buildBSTfromSortedVector(arr, st, mid - 1);
+    curr->right = buildBSTfromSortedVector(arr, mid+1, end);
+
+    return curr;
+}
+
+void getInorder(Node* root, vector<int> &nodes) {
+    if(root == NULL) {
+        return;
+    }
+
+    getInorder(root->left, nodes);
+    nodes.push_back(root->data);
+    getInorder(root->right, nodes);
+}
+
+Node* balancedBst(Node* root) {
+    //get inorder seq
+    vector<int> nodes;
+
+    getInorder(root, nodes);
+
+    return buildBSTfromSortedVector(nodes, 0, nodes.size() - 1);
+    
+}
+
+int main() {
+    // int arr[7] = {3, 4, 5, 6, 7, 8, 9};
+
+    // Node* root = buildBSTfromSorted(arr, 0, 6);
+    // preorder(root);
+    // cout << endl;
+
+    Node* root =  new Node(6);
+    root->left = new Node(5);
+    root->left->left = new Node(4);
+    root->left->left->left = new Node(3);
+
+    root->right = new Node(7);
+    root->right->right = new Node(8);
+    root->right->right->right = new Node(9);
+
+    root = balancedBst(root);
     preorder(root);
     cout << endl;
-
     return 0;
 }
